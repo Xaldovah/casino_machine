2368cef149b588fd3c8c377d26bc45c77d74d521
Dennis Kimani <dxaldovah@gmail.com>
2023-07-06 05:26:07 -0700

    The first step to a good casino machine

---
+++ machine.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+MAX_LINES = 3
+
+def deposit():
+    while True:
+        amount = input("How much do you want to deposit? $")
+
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than 0.")
+        else:
+            print("Please enter a number.")
+
+    return amount
+
+def get_number_of_lines():
+    while True:
+        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter a valid number of lines.")
+        else:
+            print("Please enter a number.")
+
+    return lines
+
+def main():
+    balance = deposit()
+    lines = get_number_of_lines()
+    print("Balance:", balance, "lines:", lines)
+
+main()
